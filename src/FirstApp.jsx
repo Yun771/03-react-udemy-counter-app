ed6d5401ea42e415f92c83ec5da16dfd339175e7
emartinez <ever.martinez@grupotum.com>
2023-01-17 17:32:45 -0600

    estilos y componentes react

---
+++ src/FirstApp.jsx
@@ -0,0 +1,10 @@
+
+export const FirstApp = () => {
+  return (
+    <>
+      <h1>Ever</h1>
+      {/* <h1>{newMessage}</h1> */}
+      <p>Soy un subtitulo</p>
+    </>
+  );
+};
---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+// export function App() {
+//   return <h1>Hola Mundo</h1>;
+// }
+
+import React from "react";
+
+export const HelloWorldApp = () => {
+  return <h1>Hello Worl </h1>;
+};
+
+// export const HelloWorldApp = () => <h1>Hello World </h1>;
